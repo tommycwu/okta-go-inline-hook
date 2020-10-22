b18dcacaba3536b11e6e44e793308e1567b4d47c
tommycwu <tommycwu@yahoo.com>
2020-10-21 23:09:26 -0700

    Initial commit

---
+++ event.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func reqHandler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	var body = `{"commands": [{"type":"com.okta.assertion.patch","value":[{"op":"add","path":"/claims/foo","value":{"attributes":{"NameFormat":"urn:oasis:names:tc:SAML:2.0:attrname-format:basic"},"attributeValues": [{"attributes":{"xsi:type":"xs:string"},"value":"barer"}]}}]}]}`
+
+	resp := events.APIGatewayProxyResponse{
+		StatusCode:      200,
+		IsBase64Encoded: false,
+		Body:            body,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}
+
+	return resp, nil
+}
+
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
+}
+
+func main() {
+	lambda.Start(reqHandler)
+}
